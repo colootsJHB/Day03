52728551ce79f80033b83b4c51e0efee4df55eed
colootsJHB <67953621+colootsJHB@users.noreply.github.com>
2020-07-13 11:57:02 +0200

    Complete.

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -10,9 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strlen(const char *str)
+int	ft_strlen(const char *str)
 {
-    int		i;
+	int	i;
 	
 	i = 0;
 	while (str[i])
